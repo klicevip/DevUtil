593b4a59b1a1cbc8a08142141f0bc75e1296a21d
xieshengxin <xieshengxin@mallcoo.cn>
2016-10-17 10:00:30 +0800

    添加忽略异常获取锁

---
+++ src/Panda.DevUtil/Distributed/LockExtension.cs
@@ -0,0 +1,26 @@
+﻿using Panda.DevUtil.Distributed.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panda.DevUtil.Distributed
+{
+    public static class LockExtension
+    {
+        public static ILockItem UsingIgnoreException(this ILock locker, string resourceId, int expire, GetLockOption option = null)
+        {
+            ILockItem lockItem = null;
+            try
+            {
+                lockItem = locker.Using(resourceId, expire, option);
+            }
+            catch
+            {
+                lockItem = new LockItem() { ResourceId = resourceId, LockId = "exception" };
+            }
+            return lockItem;
+        }
+    }
+}
